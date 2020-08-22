528c31237015808574735e8678a84e2984918873
Robert Culling <rhsculling@gmail.com>
2020-08-22 19:44:12 +1200

    Connected the write functions to the Project Manager Console.

--- PMconsole.py
+++ PMconsole.py
@@ -1,12 +1,28 @@
 # Project manager console.
 import os
 import sqlite3
+
+# Load in the classes required to define the different modules of the console.
 from PMconsole_library import HomeScreen
 from PMconsole_library import LookUp
+from PMconsole_library import InsertData
 from PMconsole_library import Close
 
+# Load in the functions required to write and read data.
+from write_database import InsertProject
+from write_database import InsertStep
+
+# Some defintions for creating readable output.
+x = "|||"
+y = "-"
+z = " "
+w = "\n"
 
 if __name__ == "__main__":
+
+    # Check if the database exists. If it does, then pass through.
+    # Otherwise run the create_database.py file to create the database
+    # for this console to interact with.
 
     # Provide a connection to the database.
     conn = sqlite3.connect("CodeProjects.db")
@@ -26,10 +42,14 @@ if __name__ == "__main__":
         if user_state.option == "":
             user_option = user_state.options()
         else:
-            # In this case, there are no options, so we can simply give the
-            # content the user has asked for.
+            # In this case there are no options to present, as the user has
+            # already specified what they want to see.
             if user_state.option == "read":
                 user_state.content()
+
+                # Once the content has been given, we can output the
+                # option for where the user wants to go next and update
+                # the state, ready for the piece of code.
 
         # At this point the user has chosen an option. We need to act on the option
         # But this action depends on the state of the user i.e. where they are in the console.
@@ -38,7 +58,49 @@ if __name__ == "__main__":
             # In this case, the user has chosen an option on the HomeScreen()
             if user_option == "L":
                 user_state = LookUp()
+            elif user_option == "I":
+                user_state = InsertData()
             else:
+                user_state = Close()
+
+
+        # In this case, the user has chosen an option on the InsertData() module.
+        elif user_state.state == "InsertData":
+
+            if user_option == "P":
+                # The user wants to write a new project.
+                continue_writing = True                     # Loop until all projects written.
+                while continue_writing == True:
+                    print(w*2)
+                    InsertProject(conn)
+                    print(w)
+                    user_continue = input("Would you like to write more projects? [Y/n] ")
+                    if user_continue == "n":
+                        continue_writing = False
+                        user_state = HomeScreen()           # Once projects are written
+                    else:                                   # User is retured to HomeScreen()
+                        pass
+
+            elif user_option == "S":
+                # The user wants to write a new step.
+                continue_writing = True                     # Loop until all steps written.
+                while continue_writing == True:
+                    print(w*2)
+                    InsertStep(conn)
+                    print(w)
+                    user_continue = input("Would you like to write more steps? [Y/n] ")
+                    if user_continue == "n":
+                        continue_writing = False
+                        user_state = HomeScreen()           # Once steps are written
+                    else:                                   # User is retured to HomeScreen()
+                        pass
+
+            elif user_option == "H":
+                # In this case, the user wants to return to the homescreen.
+                user_state = HomeScreen()
+
+            else:
+                # Otherwise, the user wants to quit the console.
                 user_state = Close()
 
         # For now, just close the console. Adjust this when other parts of the
--- PMconsole_library.py
+++ PMconsole_library.py
@@ -82,7 +82,7 @@ class HomeScreen():
 
         prompt = "Which of the following options would you like to perform?"
 
-        option_l = "[L]: Look up a project."
+        option_l = "[L]: Look up the projects."
         option_u = "[U]: Update a project."
         option_i = "[I]: Insert a new project or step into a project."
         option_r = "[R]: Remove a project."
@@ -198,7 +198,136 @@ class LookUp():
         # Once the content has been given, we need to provide
         # options for the user to continue moving around the console.
 
+    def options(self):
 
+        """
+            Options for this class will be given at the bottom of the
+            printed database. Rather than as the initial output, as is the
+            case with the other modules.
+
+        """
+
+        # shortcuts for aesthetic use.
+        x = "|||"
+        y = "-"
+        z = " "
+        w = "\n"
+
+        
+
+
+
+    def continue_program(self):
+
+        """
+            This method is used to keep track of whether or not the user
+            is finished accessing the database and wishes to close the console.
+
+        """
+
+        return True
+
+class InsertData():
+
+    """
+        This class is used to implement the code for writing projects
+        to the CodeProject.db database.
+
+    """
+
+    def __init__(self):
+
+        """
+            This class is used to define the homescreen of the project
+            manager console. This class stores the options available to the
+            user at the homescreen.
+
+        """
+
+        self.state = "InsertData"
+        self.option = ""
+
+    def header(self):
+
+        """
+            This method prints the header for the write data section of the
+            project manager console.
+
+        """
+
+        x = "|||"
+        y = "-"
+        z = " "
+        w = "\n"
+
+        welcome_message = " Project Manager Console: Write Data "
+        l_welcome = len(welcome_message)
+
+        print(x + 205*y + x)
+        print(3*w)
+
+        print(z*75 + x + l_welcome*y + x)
+        print(z*75 + x + welcome_message + x)
+        print(z*75 + x + l_welcome*y + x)
+
+    def explanation(self):
+
+        """
+            This method provides the user with an explanation of the
+            types of data that can be written to the project manager
+            CodeProjects.db database.
+
+        """
+
+        x = "|||"
+        y = "-"
+        z = " "
+        w = "\n"
+
+        explanation_message_1 = z*10 + "This module of the console lets the user write data to the CodeProject.db database."
+        explanation_message_2 = z*10 + "You may write a project, or a step into a project."
+        print(w*3)
+        print(explanation_message_1)
+        print(explanation_message_2)
+
+
+    def options(self):
+
+        """
+
+        """
+
+        # shortcuts for aesthetic use.
+        x = "|||"
+        y = "-"
+        z = " "
+        w = "\n"
+
+        prompt = "Choose whether you would like to write a new project or a step into an exisiting project."
+
+        option_p = "[P]: Write a new project."
+        option_s = "[S]: Write a new step into a project."
+        option_h = "[H]: Return to the home screen."
+        option_q = "[Q]: Quit the project manager console."
+
+        options = [option_p,
+                   option_s,
+                   option_h,
+                   option_q]
+
+        # Start printing the options for the user.
+        print(w*2)
+        print(z*15 + prompt + w)
+        for o in options:
+            print(z*20 + o)
+
+        print(w)
+        user_option = input(z*15 + "Option: ")
+
+        # Update the option attribute of this class.
+        self.option = user_option
+
+        return user_option
 
     def continue_program(self):
 
@@ -216,6 +345,18 @@ class Close():
         This class is used to close the program and print an exit statement.
 
     """
+
+    def __init__(self):
+
+        """
+            This class is used to define the homescreen of the project
+            manager console. This class stores the options available to the
+            user at the homescreen.
+
+        """
+
+        self.state = "Close"
+        self.option = "Quit"
 
     def header(self):
 
